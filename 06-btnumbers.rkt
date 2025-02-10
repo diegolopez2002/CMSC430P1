0dc8a5e3e63bd788728d2c229f4dc479be6d7d54
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2025-02-10 17:54:50 -0500

    Update 06-btnumbers.rkt

--- 06-btnumbers.rkt
+++ 06-btnumbers.rkt
@@ -6,35 +6,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Binary trees of numbers
 
-;; Write test cases for each function (before writing code!).
-
-;; type BTNumber =
-;; | (leaf)
-;; | (node Number BTNumber BTNumber)
-
 ;; We will represent Binary trees in racket with structures.
 (struct leaf ()             #:prefab)
 (struct node (n left right) #:prefab)
-;; this structure 'node' should be recursive -
-;; it's last two parameters should be binary trees
-
-;; Follow this template for functions on binary trees.
-;; bt ... -> ...
-#;
-(define (btn-template n)
-  (match n
-    [(leaf) ...]
-    [(node n left right)
-     (... n
-          (btn-template left ...)
-          (btn-template right ...) ...)]))
-
 
 ;; BTNumber -> Natural
 ;; Compute the height of a binary tree (leaf has height 0)
 (define (btn-height bt)
-  ;; TODO
-  0)
+  (match bt
+    [(leaf) 0]
+    [(node _ left right) (add1 (max (btn-height left) (btn-height right)))]))
 
 (module+ test
   (check-equal? (btn-height (leaf)) 0)
@@ -44,8 +25,9 @@
 ;; BTNumber -> Natural
 ;; Count the nodes of a binary tree
 (define (btn-count bt)
-  ;; TODO
-  0)
+  (match bt
+    [(leaf) 0]
+    [(node _ left right) (+ 1 (btn-count left) (btn-count right))]))
 
 (module+ test
   (check-equal? (btn-count (leaf)) 0)
@@ -55,8 +37,9 @@
 ;; BTNumber -> BTNumber
 ;; Compute the mirror image of binary tree
 (define (btn-mirror bt)
-  ;; TODO
-  (leaf))
+  (match bt
+    [(leaf) (leaf)]
+    [(node n left right) (node n (btn-mirror right) (btn-mirror left))]))
 
 (module+ test
   (check-equal? (btn-mirror (leaf)) (leaf))
@@ -67,8 +50,9 @@
 ;; BTNumber -> Number
 ;; Sum the numbers of a binary tree
 (define (btn-sum bt)
-  ;; TODO
-  0)
+  (match bt
+    [(leaf) 0]
+    [(node n left right) (+ n (btn-sum left) (btn-sum right))]))
 
 (module+ test
   (check-equal? (btn-sum (leaf)) 0)
@@ -76,10 +60,11 @@
   (check-equal? (btn-sum (node 5 (node 1 (leaf) (leaf)) (leaf))) 6))
 
 ;; Natural Number -> BTNumber
-;; Generate a full bt of height h containing given number n at each node
+;; Generate a full binary tree of height h containing given number n at each node
 (define (btn-gen-full h n)
-  ;; TODO
-  (leaf))
+  (if (= h 0)
+      (leaf)
+      (node n (btn-gen-full (- h 1) n) (btn-gen-full (- h 1) n))))
 
 (module+ test
   (check-equal? (btn-gen-full 0 8) (leaf))
@@ -87,10 +72,12 @@
   (check-equal? (btn-gen-full 2 8) (node 8 (node 8 (leaf) (leaf)) (node 8 (leaf) (leaf)))))
 
 ;; BTNumber Number -> Boolean
-;; Does the bt contain number n?
+;; Does the binary tree contain number n?
 (define (btn-contains? bt n)
-  ;; TODO
-  #f)
+  (match bt
+    [(leaf) #f]
+    [(node m left right)
+     (or (= m n) (btn-contains? left n) (btn-contains? right n))]))
 
 (module+ test
   (check-equal? (btn-contains? (leaf) 8) #f)
@@ -99,11 +86,11 @@
   (check-equal? (btn-contains? (node 5 (leaf) (node 8 (leaf) (leaf))) 8) #t))
 
 ;; BTNumber -> [Listof Number]
-;; Generate the list of numbers in bt in preorder
-;; HINT: append is a function that might be helpful
-(define (btn-preorder btn)
-  ;; TODO
-  '())
+;; Generate the list of numbers in binary tree in preorder
+(define (btn-preorder bt)
+  (match bt
+    [(leaf) '()]
+    [(node n left right) (append (list n) (btn-preorder left) (btn-preorder right))]))
 
 (module+ test
   (check-equal? (btn-preorder (leaf)) '())
